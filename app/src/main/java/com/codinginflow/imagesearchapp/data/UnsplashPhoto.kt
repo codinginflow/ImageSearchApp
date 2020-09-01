f5a0707bece620166105f13fb58fa83016e6db22
Florian <info@codinginflow.com>
2020-09-01 02:29:24 -0700

    End of part 3

--- app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashPhoto.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashPhoto.kt
@@ -1,4 +1,4 @@
-package com.codinginflow.imagesearchapp
+package com.codinginflow.imagesearchapp.data
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.codinginflow.imagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.codinginflow.imagesearchapp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
