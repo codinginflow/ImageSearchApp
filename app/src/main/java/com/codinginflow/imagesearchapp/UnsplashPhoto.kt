72df1b6810a1e46e7767107e7c193474060a7163
Florian <info@codinginflow.com>
2020-09-01 10:43:48 -0700

    End of part 2

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/UnsplashPhoto.kt
@@ -0,0 +1,30 @@
+package com.codinginflow.imagesearchapp
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class UnsplashPhoto(
+    val id: String,
+    val description: String?,
+    val urls: UnsplashPhotoUrls,
+    val user: UnsplashUser
+) : Parcelable {
+
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val raw: String,
+        val full: String,
+        val regular: String,
+        val small: String,
+        val thumb: String,
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        val name: String,
+        val username: String
+    ) : Parcelable {
+        val attributionUrl get() = "https://unsplash.com/$username?utm_source=ImageSearchApp&utm_medium=referral"
+    }
+}
