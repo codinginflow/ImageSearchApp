d1519c56984b10fc583bf5508ae70137e4c62c82
Florian <info@codinginflow.com>
2020-09-05 08:23:38 -0700

    End of part 8

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -25,7 +25,10 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
         binding.apply {
             recyclerView.setHasFixedSize(true)
-            recyclerView.adapter = adapter
+            recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
+                footer = UnsplashPhotoLoadStateAdapter { adapter.retry() },
+            )
         }
 
         viewModel.photos.observe(viewLifecycleOwner) {
---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/UnsplashPhotoLoadStateAdapter.kt
@@ -0,0 +1,45 @@
+package com.codinginflow.imagesearchapp.ui.gallery
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.codinginflow.imagesearchapp.databinding.UnsplashPhotoLoadStateFooterBinding
+
+class UnsplashPhotoLoadStateAdapter(private val retry: () -> Unit) :
+    LoadStateAdapter<UnsplashPhotoLoadStateAdapter.LoadStateViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, loadState: LoadState): LoadStateViewHolder {
+        val binding = UnsplashPhotoLoadStateFooterBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return LoadStateViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: LoadStateViewHolder, loadState: LoadState) {
+        holder.bind(loadState)
+    }
+
+    inner class LoadStateViewHolder(private val binding: UnsplashPhotoLoadStateFooterBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.buttonRetry.setOnClickListener {
+                retry.invoke()
+            }
+        }
+
+        fun bind(loadState: LoadState) {
+            binding.apply {
+                progressBar.isVisible = loadState is LoadState.Loading
+                buttonRetry.isVisible = loadState !is LoadState.Loading
+                textViewError.isVisible = loadState !is LoadState.Loading
+            }
+        }
+    }
+}
