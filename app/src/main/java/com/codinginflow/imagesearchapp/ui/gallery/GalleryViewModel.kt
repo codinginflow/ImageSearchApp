e6437a2802f487c42b3e9396b9a1d8ea2e84725d
Florian <info@codinginflow.com>
2020-09-06 11:41:24 -0700

    End of part 13

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryViewModel.kt
@@ -1,18 +1,17 @@
 package com.codinginflow.imagesearchapp.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.codinginflow.imagesearchapp.data.UnsplashRepository
 
 class GalleryViewModel @ViewModelInject constructor(
-    private val repository: UnsplashRepository
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
 ) : ViewModel() {
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     val photos = currentQuery.switchMap { queryString ->
         repository.getSearchResults(queryString).cachedIn(viewModelScope)
@@ -23,6 +22,7 @@ class GalleryViewModel @ViewModelInject constructor(
     }
 
     companion object {
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "cats"
     }
 }
